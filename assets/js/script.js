b46eaa1285e9e87a4cb99bd1ad5061be11746b31
John-Pena <jmp1495@gmail.com>
2022-03-12 11:50:56 -0600

    added color to hour slots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
-var tasks = [];
-
 // call this function to pass the date
 $(document).ready(function () {
     $('#currentDay').text(moment().format('ddd MMM Do YYYY'));
+});
+
+
+console.log($('[data-time]')) 
+$('[data-time]').each(function(index) {
+    console.log(index + ": " + $(this).attr('id'));
+    if (moment().hour() > parseInt($(this).attr('data-time'))) {
+        $(this).parent().next().addClass('past');
+    }
+
+    else if (moment().hour() === parseInt($(this).attr('data-time'))) {
+        $(this).parent().next().addClass('present');
+    }
+
+    else {
+        $(this).parent().next().addClass('future');
+    }
+ 
 });
 
 $('.saveBtn').click(function() {
